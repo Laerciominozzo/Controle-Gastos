d8d720c127b3bdcc0881b6a052d3b3cb0974f79e
lminozzo <laerciominozzo@hotmail.com>
2018-11-10 11:22:29 -0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+
+extern crate rocket;
+extern crate rocket_contrib;
+
+use rocket::response::NamedFile;
+use rocket_contrib::Template;
+use std::path::Path;
+use std::path::PathBuf;
+
+mod controller;
+
+
+#[get("/<file..>", rank = 10)] // set the rank higher than other endpoints
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("templates/").join(file)).ok()
+}
+
+
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![controller::table::index, files])
+        .mount("/form", routes![controller::form::form])
+        .attach(Template::fairing())
+        .launch();
+}
