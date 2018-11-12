9dc768e494e98da054d0ee3efd58e696ff6cfa92
lminozzo <laerciominozzo@hotmail.com>
2018-11-11 22:54:27 -0200

    Colocados todos os codigos em um unico arquivo.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,37 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
-
+#![feature(custom_derive)]
 
 extern crate rocket;
 extern crate rocket_contrib;
+extern crate time;
 
+use rocket::request::Form;
 use rocket::response::NamedFile;
 use rocket_contrib::Template;
+use std::collections::HashMap;
 use std::path::Path;
 use std::path::PathBuf;
 
-mod controller;
 
+#[derive(FromForm)]
+struct Gasto {
+    valor: f32,
+    tipo: u8,
+    descricao: String,
+    idUsuario: u32,
+}
+
+
+#[get("/newCusto")]
+fn new_custo() -> Template {
+    Template::render("form", HashMap::<String, String>::new())
+}
+
+#[post("/cadastroCusto", data = "<custo>")]
+fn cadastro_custo(custo: Form<Gasto>) -> Template {
+    Template::render("form", HashMap::<String, String>::new())
+}
 
 #[get("/<file..>", rank = 10)] // set the rank higher than other endpoints
 fn files(file: PathBuf) -> Option<NamedFile> {
@@ -19,9 +39,15 @@ fn files(file: PathBuf) -> Option<NamedFile> {
 }
 
 
+#[get("/")]
+fn index() -> Template {
+    Template::render("tables", HashMap::<String, String>::new())
+}
+
+
 fn main() {
     rocket::ignite()
-        .mount("/", routes![controller::table::index, files, controller::form::form])
+        .mount("/", routes![index, files, new_custo, cadastro_custo])
         .attach(Template::fairing())
         .launch();
 }
